21eebdd0025566a733c4ddb627dda5b0e879c32c
nhngoccham <nhnt1811@gmail.com>
2024-07-11 00:08:08 +0700

    update home message

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Learning Analytics Platform"));
 
 // Sử dụng router được định nghĩa trong router.js
 require("../routers/index")(app);
